e05c941b2c3a8d9230bc6d6e9c87f6531073d00e
eduzintchutchuco <141173038+mavyrmldy@users.noreply.github.com>
2024-02-16 07:26:46 -0300

    Initial commit

---
+++ app/src/main/java/primeiro_exemplo/Hello.java
@@ -0,0 +1,11 @@
+package primeiro_exemplo;
+
+public class Hello {
+    public static void main(String[] args) {
+        int cont = 10;
+        String nome = "Marco";
+        System.out.println("Olá Mundo!!!");
+
+        System.out.println(cont + " : " + nome);
+    }
+}
